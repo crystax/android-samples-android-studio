c8249a9d93ea6687e61e25e56dc587132176fe82
Dmitry Moskalchuk <dm@crystax.net>
2015-12-11 18:50:27 +0300

    Enable full Boost build
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

---
+++ app/src/main/jni/test.cpp
@@ -0,0 +1,56 @@
+#include <jni.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <string>
+#include <exception>
+#include <sstream>
+
+#include <android/log.h>
+
+#define LOG(fmt, ...) __android_log_print(ANDROID_LOG_INFO, "TEST-BOOST", fmt, ##__VA_ARGS__)
+
+#include "gps.hpp"
+
+std::string gps(std::string const &root)
+{
+    const gps_position g(35, 59, 24.567f);
+    save(root, g);
+
+    gps_position newg;
+    load(root, newg);
+
+    std::ostringstream ostr;
+
+    if (g != newg)
+    return std::string();
+
+    ostr << "GPS coordinates: " << newg;
+    return ostr.str();
+}
+
+extern "C"
+jstring
+Java_net_crystax_examples_testboost2_MainActivity_getGPSCoordinates( JNIEnv* env,
+                                                                    jobject thiz,
+                                                                    jstring rootPath )
+{
+    const char *s = env->GetStringUTFChars(rootPath, 0);
+    std::string root(s);
+    env->ReleaseStringUTFChars(rootPath, s);
+    LOG("root: %s", root.c_str());
+
+    try {
+        std::string ret = gps(root);
+        return env->NewStringUTF(ret.c_str());
+    }
+    catch (std::exception &e) {
+        LOG("ERROR: %s", e.what());
+        abort();
+    }
+    catch (...) {
+        LOG("Unknown error");
+        abort();
+    }
+}
