2b39491bc3f82b276e8eb6746f00be9c1ea4366a
Dmitry Moskalchuk <dm@crystax.net>
2016-05-23 08:34:50 +0300

    Add explicit loading of Boost library in static initialization block
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- app/src/main/java/net/crystax/examples/testboost2/MainActivity.java
+++ app/src/main/java/net/crystax/examples/testboost2/MainActivity.java
@@ -5,15 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     static {
         System.loadLibrary("crystax");
+        System.loadLibrary("boost_serialization");
         System.loadLibrary("test-boost2");
     }
 
